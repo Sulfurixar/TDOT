7ed532bcd54086accca79c159a72f988ff91ad9b
Sulfurixar <Sulfurixar@gmail.com>
2018-02-21 21:23:56 +0200

    DEV_BACKUP_54
    TDOT

--- TDOT.py
+++ TDOT.py
@@ -363,9 +363,11 @@ def on_message_delete(msg):
         s = d.servers[s]
         if s.message_delete_logging and s.delete_channel != ['', ''] and s.active and s.delete_channel[2] != msg.channel:
             if msg.author.id in d.dmessages:
-                if msg.type == type(d.dmessages[msg.author.id]):
-                    if msg.content == d.dmessages[msg.author.id].content:
-                        return
+                if len(d.dmessages[msg.author.id]) > 0:
+                    for message in d.dmessages[msg.author.id]:
+                        if msg.type == type(message):
+                            if msg.content == message.content:
+                                return
             embed = d.embedder([[
                 '**Detected message deletion:**', 'Deleted message owned by: <@' + msg.author.id + '> ' +
                 msg.author.display_name + ':' + msg.author.id + '\nContents: \'' + msg.content + '\'\nTime: ' +
